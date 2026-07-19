374804104ba5c27273a9f4b5c1c57af417559a86
Muhammad Ahmad Zaheer <ahmedzaheer2004.24@gmail.com>
2026-07-19 14:08:57 +0500

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+let tasks = [
+  {
+    id: 1,
+    title: "Learn Express",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "Build a CRUD API",
+    done: false,
+  },
+  {
+    id: 3,
+    title: "Test API endpoints",
+    done: true,
+  },
+];
+
 app.get("/", (req, res) => {
   res.json({
     name: "Task API",
@@ -15,6 +33,24 @@ app.get("/health", (req, res) => {
   res.json({
     status: "ok",
   });
+});
+
+app.get("/tasks", (req, res) => {
+  res.status(200).json(tasks);
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const taskId = Number(req.params.id);
+
+  const task = tasks.find((task) => task.id === taskId);
+
+  if (!task) {
+    return res.status(404).json({
+      error: `Task ${taskId} not found`,
+    });
+  }
+
+  res.status(200).json(task);
 });
 
 app.listen(PORT, () => {
